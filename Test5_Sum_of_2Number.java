652ee20a834d18b565cb76147c772f37e0682d53
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-15 17:40:47 +0530

    Java Program - Sum of two Numbers

---
+++ Test5_Sum_of_2Number.java
@@ -0,0 +1,35 @@
+package com.test.Basic_Java_Programs;
+
+import java.util.Scanner;
+
+public class Test5_Sum_of_2Number 
+{
+	public static void main(String[] args) 
+	{
+		int x, y, sum;
+		
+		System.out.println("Enter two Numbers");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		x = sc.nextInt();	
+		y = sc.nextInt();
+		
+		sum = x + y;
+		
+		System.out.println("Addition of two Number is " + sum);
+	}
+
+}
+
+//Output
+/* 
+ 
+ Enter two Numbers
+10
+20
+Addition of two Number is 30
+ 
+  
+ * 
+ */
