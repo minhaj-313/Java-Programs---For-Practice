a77d1d51aa8fe3cc499102b12444bb21f022d257
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-15 18:13:48 +0530

    Java Program - Print Even Numbers in Given Range

---
+++ Test9_Even_number_inRange.java
@@ -0,0 +1,44 @@
+//Print Even Numbers in Given Range
+
+package com.test.Basic_Java_Programs;
+
+import java.util.Scanner;
+
+public class Test9_Even_number_inRange 
+{
+		public static void main(String[] args) 
+		{
+			int n;
+			
+			System.out.print("Enter Range :");
+			
+			Scanner sc = new Scanner(System.in);
+			
+			n = sc.nextInt();		
+			
+			for (int i = 0; i <= n; i=i+2) 
+			{
+				System.out.println(i);
+				
+			}
+		}
+
+}
+
+
+//Output:
+/*
+
+Enter Range :10
+0
+2
+4
+6
+8
+10
+
+
+
+
+
+*/
