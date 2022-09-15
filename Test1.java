f647e98ef9db5337e7afb0c6ed88f2b609716d8e
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-15 15:46:39 +0530

    Java -  Hello World Program
    Program 1

---
+++ Test1.java
@@ -0,0 +1,22 @@
+package com.test.Basic_Java_Programs;
+
+public class Test1 
+{
+	
+//Main Method : Every Program Execution Starts from Main Method
+	public static void main(String[] args) 
+	{
+		//For Printing Statement we make use of "System.out.print();"
+		System.out.println("Hello World");
+	}
+
+}
+
+/* OUTPUT :
+*
+* 
+*     Hello World
+*
+*
+*/
+
