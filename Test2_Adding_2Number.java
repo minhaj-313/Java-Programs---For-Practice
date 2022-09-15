f0e446d80a526438528e03695db6885b4a30e014
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-15 17:16:26 +0530

    Add files via upload

---
+++ Test2_Adding_2Number.java
@@ -0,0 +1,22 @@
+package com.test.Basic_Java_Programs;
+
+public class Test2_Adding_2Number 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 20;
+		
+		int c = a + b;
+		
+		System.out.println("Sum of two Number is " + c);
+		
+	}
+
+}
+
+/* Output :
+
+  30
+
+*/
---
+++ Test3_User_Input.java
@@ -0,0 +1,34 @@
+package com.test.Basic_Java_Programs;
+
+import java.util.Scanner;
+
+public class Test3_User_Input 
+{
+	public static void main(String[] args) 
+	{
+		int a;
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the Value : ");
+		
+		a = sc.nextInt();
+		
+		System.out.println(a);
+		
+	
+	}
+
+}
+
+// Output:
+/*
+  
+ 
+Enter the Value : 100
+100
+
+ 
+ 
+ 
+ */
