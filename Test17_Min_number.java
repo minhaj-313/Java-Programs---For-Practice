98192e4c4c2675f345458ba6fca291e2dbf9c276
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-16 22:41:28 +0530

    Java Program - Minimum Number between 2 number

---
+++ Test17_Min_number.java
@@ -0,0 +1,28 @@
+// Minimum number between two Numbers
+package com.test.Basic_Java_Programs;
+
+public class Test17_Min_number 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10, b = 20;
+		
+		if (a < b) 
+		{
+			System.out.println(a);
+		} 
+		else 
+		{
+			System.out.println(b);
+		}
+		
+	}
+
+}
+
+
+/* Output:
+ 
+10
+
+*/
