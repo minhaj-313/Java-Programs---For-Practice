efaa2dcc053ce0f3d204538e8d568885e0cb3467
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-16 22:27:05 +0530

    Java Program - Print A to Z alphabets

---
+++ Test14__A_to_Z_programs.java
@@ -0,0 +1,23 @@
+package com.test.Basic_Java_Programs;
+
+public class Test14__A_to_Z_programs 
+{
+	public static void main(String[] args) 
+	{
+		for (char i = 'A'; i <= 'Z'; i++) 
+		{
+			System.out.print(i + " ");
+			
+		}
+		
+	}
+
+}
+
+
+/*  Output:
+
+A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
+
+
+*/
