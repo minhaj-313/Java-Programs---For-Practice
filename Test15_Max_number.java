2db7b19241555e89f6c1a8a4af40e110ec1fb4ef
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-16 22:32:45 +0530

    Java Program - Maximum Number between 2 number
    Java Program - Maximum Number between 2 number

---
+++ Test15_Max_number.java
@@ -0,0 +1,28 @@
+// Maximum number between two Numbers
+package com.test.Basic_Java_Programs;
+
+public class Test15_Max_number 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10, b = 20;
+		
+		if (a > b) 
+		{
+			System.out.println(a);
+		} 
+		else 
+		{
+			System.out.println(b);
+		}
+		
+	}
+
+}
+
+
+/* Output:
+ 
+20
+
+*/
