c75ea697a7e24e7b9c3cc133e14ad4c820831f3d
Shaikh Minhaj <107562768+minhaj-313@users.noreply.github.com>
2022-09-19 13:22:12 +0530

    Java Programs - Greatest Number among 3

---
+++ Test20_Greatest_number_amoung3.java
@@ -0,0 +1,47 @@
+package com.test.Basic_Java_Programs;
+
+public class Test20_Greatest_number_amoung3 
+{
+	public static void main(String[] args) 
+	{
+		int a=10, b=50, c= 30;
+		
+		if (a>b) 
+		{
+			if (a>c) 
+			{
+				System.out.println(a);
+				
+			}
+			else 
+			{
+				System.out.println(c);
+				
+			}
+			
+		}
+		else 
+		{
+			if (b>c) 
+			{
+				System.out.println(b);
+				
+			} 
+			else 
+			{
+				System.out.println(c);
+
+			}
+			
+		}
+	}
+
+}
+
+
+/* Output:
+ * 
+ * 			
+ * 	50
+ * 
+ */
